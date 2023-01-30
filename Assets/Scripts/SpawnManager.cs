43d9be21a670bdf8a9ac9393e273a980745aef5d
dcalitri <dcalitri@student.fitchburgstate.edu>
2023-01-29 22:08:45 -0500

    Made the enemy spawn in random positions on the floor.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,10 +8,19 @@ public class SpawnManager : MonoBehaviour
     private int numOfEnemiesLeft;
     private int numOfEnemies;
     public GameObject enemy;
+    private float spawnRange = 9;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(enemy, GenerateSpawnPosition(), enemy.transform.rotation);
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 1, spawnPosZ);
+        return randomPos;
     }
 
     // Update is called once per frame
