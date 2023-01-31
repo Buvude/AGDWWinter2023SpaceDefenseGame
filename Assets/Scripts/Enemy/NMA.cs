aae7f161ad658676da18475140b5fcb7a5583e6f
Buvude <90856761+Buvude@users.noreply.github.com>
2023-01-31 09:43:56 -0500

    implimented begining of navmesh

--- Assets/Scripts/Enemy/NMA.cs
+++ Assets/Scripts/Enemy/NMA.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NMA : MonoBehaviour
 {
+    public enum EnemyState { Searching, Chasing, Attacking, Dead}; //depending on the state the enemy is in, it will act differently
+    public Transform CurrentTarget; //setting this to the navmesh agent's goal in update so it can be changed
+    public NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.destination = CurrentTarget.position;
     }
 }
