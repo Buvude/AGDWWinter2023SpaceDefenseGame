311241e75ffa623163e35db1239d2003b1061fcc
dcalitri <dcalitri@student.fitchburgstate.edu>
2023-01-31 11:34:17 -0500

    Making enemy spawn at a random spawn point Attempt 1

--- Assets/Scripts/Enemy/SpawnManager.cs
+++ Assets/Scripts/Enemy/SpawnManager.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     private int maxNumOfEnemies;
+    public GameObject[] spawnPoints;
     private int numOfEnemiesLeft;
     private int numOfEnemies = 5;
     public GameObject enemy;
+    private Vector3 whereToSpawn;
     public int enemyCount;
     private float spawnRange = 9;
     public int waveNumber = 1;
+
     // Start is called before the first frame update
     void Start()
     {
         SpawnEnemyWave(numOfEnemies + waveNumber);
+        Vector3 whereToSpawn = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
     }
     //This will probably need to change to specific spawn points once we have the map implimented, but good start
     private Vector3 GenerateSpawnPosition()
@@ -39,7 +43,7 @@ public class SpawnManager : MonoBehaviour
     {
         for (int i = 0; i < enemiesToSpawn; i++)
         {
-            Instantiate(enemy, GenerateSpawnPosition(), enemy.transform.rotation);
+            Instantiate(enemy, whereToSpawn, enemy.transform.rotation);
         }
     }
 }
