5029cc6c3565e34a63081f94b14d381b4b0b8793
Buvude <90856761+Buvude@users.noreply.github.com>
2023-01-31 10:15:18 -0500

    very basic enemy AI implimented

--- Assets/Scripts/Enemy/NMA.cs
+++ Assets/Scripts/Enemy/NMA.cs
@@ -6,17 +6,24 @@ using UnityEngine.AI;
 public class NMA : MonoBehaviour
 {
     public enum EnemyState { Searching, Chasing, Attacking, Dead}; //depending on the state the enemy is in, it will act differently
-    public Transform CurrentTarget; //setting this to the navmesh agent's goal in update so it can be changed
-    public NavMeshAgent agent;
+    private Vector3 CurrentTarget; 
+    private NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
+        //TEMPORARY TEST FOR NAVMESH
         agent = GetComponent<NavMeshAgent>();
+        CurrentTarget.Set(0f, 0f, 0f);
+        UpdateTarget();//putting this as a method so it can be called whenever, but not constantly every frame. This might optimize the game a bit more
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = CurrentTarget.position;
+        
+    }
+    public void UpdateTarget()
+    {
+        agent.destination = CurrentTarget;
     }
 }
