04936773e43f7c5d0d7a720f38178183455b4d01
Buvude <90856761+Buvude@users.noreply.github.com>
2023-02-06 11:19:40 -0500

    outlined code idea

---
+++ Assets/Scripts/MapNavigation/Enemy Goal Poin.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGoalPoin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //have a list of EGPs, for the enemy to select where to move randomly. At each EGP, the enemy will select from a valid EGP to go to next. 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
