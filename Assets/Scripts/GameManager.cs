bc603cf5d62fb9be63f2cf1ab617046792ca3d45
dcalitri <dcalitri@student.fitchburgstate.edu>
2023-02-12 02:45:09 -0500

    Also added some ammo variables

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,11 +7,15 @@ public class GameManager : MonoBehaviour
 {
     private int health;
     public TextMeshProUGUI healthText;
+    private int ammo;
+    public TextMeshProUGUI ammoText;
     // Start is called before the first frame update
     void Start()
     {
         health = 100;
         healthText.text = "Health: " + health + "%";
+        ammo = 6;
+        ammoText.text = "Ammo: " + ammo;
     }
 
     // Update is called once per frame
