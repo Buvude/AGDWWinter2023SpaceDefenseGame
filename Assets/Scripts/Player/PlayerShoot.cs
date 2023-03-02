5786a89e84d648a6b878a065748199cbf4e6574e
dcalitri <dcalitri@student.fitchburgstate.edu>
2023-03-02 11:47:05 -0500

    Got shoot to work

---
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public int gunDamage = 1;
+    public float fireRate = 0.25f;
+    public float range = 50f;
+
+    public Camera playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.Die();
+            }
+        }
+    }
+}
