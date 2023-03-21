af0eceafa06a99060f07cdcbdd24eb77bb4037c5
LunaFreya64 <chrisvisconte@gmail.com>
2023-03-21 14:00:21 -0400

    working on tower defense stuff

--- Assets/Scripts/Player/TowerDefenseScript.cs
+++ Assets/Scripts/Player/TowerDefenseScript.cs
@@ -6,6 +6,7 @@ public class TowerDefenseScript : MonoBehaviour
 {
     public GameObject turret;
     public GameObject mine;
+    public float scrap;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,14 @@ public class TowerDefenseScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (scrap >= 50 && Input.GetKeyDown(KeyCode.T))
+        {
+            Instantiate(turret, transform.position, transform.rotation);
+            scrap -= 50;
+        }else if (scrap >= 25 && Input.GetKeyDown(KeyCode.G))
+        {
+            Instantiate(mine, transform.position, transform.rotation);
+            scrap -= 25;
+        }
     }
 }
